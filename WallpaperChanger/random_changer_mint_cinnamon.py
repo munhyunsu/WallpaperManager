a4da750f1af6e7c9e2d3178db04290defcd14a09
munhyunsu <munhyunsu@gmail.com>
2017-03-17 10:30:28 +0900

    Version 1.0.1

---
+++ WallpaperChanger/random_changer_mint_cinnamon.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import check_ini
+
+import os.path
+import sys # exit, argv
+import logging # 로깅
+import configparser # configParser
+import sqlite3 # SQL
+import subprocess # change background
+import time # sleep
+import random # choice
+
+INIFILE = 'random_changer.ini'
+
+class RandomChanger(object):
+    """배경화면 복사 기본 클래스
+    """
+    def __init__(self, config_file):
+        """설정 파일 입력 받기
+        """
+        # 입력 파일 확인
+        if type(config_file) == dict:
+            self.config = config_file
+        else:
+            self.config = configparser.ConfigParser()
+            self.config.read(config_file)
+        # 설정 파일 확인
+        if not check_ini.check_ini(self.config):
+            sys.exit(0)
+        # 로깅 시스템 셋업
+        if not self._set_logging():
+            sys.exit(0)
+
+    def _set_logging(self):
+        """로그 시스템 셋업
+        """
+        config = self.config
+        filename = config['log']['file']
+        level = config['log']['level']
+        level = getattr(logging, level)
+        try:
+            logging.basicConfig(filename = filename,
+                                level = level,
+                                format = ('%(asctime)s '
+                                        + '%(levelname)s: '
+                                        + '%(message)s'),
+                                datefmt = '%Y.%m.%d %H:%M:%S')
+            return True
+        except Exception as err:
+            logging.critical('__set_logging 예외 발생: {0}'.format(
+                    err))
+            return False
+     
+    def _get_image_path(self):
+        wallpaper_map = \
+                self.config['wallpaper']['map'].splitlines()
+
+        while True:
+            choice_map = random.choice(wallpaper_map)
+            (database, image) = choice_map.split(' ')
+            connector = sqlite3.connect(database)
+            cursor = connector.cursor()
+            cursor.execute('SELECT md5, file_ext FROM image '
+                         + self._get_where(sys.argv)
+                         + 'ORDER BY RANDOM() '
+                         + 'LIMIT 1')
+            cursor_result = cursor.fetchone()
+            (md5, file_ext) = cursor_result
+            file_name = md5 + '.' + file_ext
+            image_path = image + '/' + file_name
+            image_path = os.path.abspath(image_path)
+            image_path = 'file://' + image_path
+            yield image_path
+
+    def _get_where(self, argv):
+        """WHERE문 제작
+        """
+        # 인자 처리
+        if len(argv) < 2:
+            rating_argv = 'sqe'
+        else:
+            rating_argv = str.join('', argv[1:])
+        # WHERE문 처리
+        result = ''
+        for rating_index in ['s', 'q', 'e']:
+            if rating_index in rating_argv:
+                if not 'WHERE' in result:
+                    result = result + 'WHERE '
+                if 'rating' in result:
+                    result = result + 'or '
+                result = result + 'rating="' + rating_index + '" '
+        # WHERE문 반환
+        return result
+
+    def start_change(self):
+        """배경화면 변경 시작
+        """
+        config = self.config
+        sleep_time = int(config['wallpaper']['sleep'])
+        image_path_generator = self._get_image_path()
+        try:
+            logging.info('배경화면 변경 시작: 종료하려면 Ctrl + C')
+            for image_path in image_path_generator:
+                subprocess.run(['gsettings',
+                                'set',
+                                'org.cinnamon.desktop.background',
+                                'picture-uri',
+                                image_path])
+                subprocess.run(['gsettings',
+                                'set',
+                                'org.cinnamon.desktop.background',
+                                'picture-opacity',
+                                '100'])
+                subprocess.run(['gsettings',
+                                'set',
+                                'org.cinnamon.desktop.background',
+                                'picture-options',
+                                'scaled'])
+                logging.info('배경화면 변경: {0}'.format(image_path))
+                time.sleep(sleep_time)
+                
+        except KeyboardInterrupt:
+            logging.info('배경화면 변경 종료')
+            return 0
+
+
+def main(argv):
+    wallpaper_random_changer = RandomChanger(INIFILE)
+    wallpaper_random_changer.start_change()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
