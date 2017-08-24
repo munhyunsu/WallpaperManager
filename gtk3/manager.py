81c7d9298591de841a63ef8bccffcf0c6508a35a
munhyunsu <munhyunsu@gmail.com>
2017-08-24 08:46:39 -0700

    수정

--- gtk3/manager.py
+++ gtk3/manager.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
-import gi
+import sys # exit
+import gi # gtk3
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+
+
+
+
+import downloader
 
 class MainMenu(Gtk.Window):
     def __init__(self):
@@ -13,9 +19,21 @@ class MainMenu(Gtk.Window):
         self.add(self.button)
 
     def on_button_clicked(self, widget):
+        image = downloader.get_image()
+        self.remove(self.button)
+        self.image = Gtk.Image()
+        self.image.set_from_file(image)
+        self.add(self.image)
+        self.show_all()
         print('Hello world')
 
-win = MainMenu()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+
+def main(argv):
+    win = MainMenu()
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+    
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
