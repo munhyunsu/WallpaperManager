9d4df7253f9751f280bebb381f6245a940e78b37
munhyunsu <munhyunsu@gmail.com>
2017-10-09 17:13:50 +0900

    중간

---
+++ gtk3/image_downloader.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# exit
+import sys
+
+def main(argv):
+    """
+    main flow
+    """
+    # TODO(LuHa): print message about program execution
+    print('[WD] Execute wallpaper downloader')
+
+    # TODO(LuHa): print message about program terminaion
+    print('[WD] Terminate wallpaper downloader')
+
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
--- gtk3/manager_cli.py
+++ gtk3/manager_cli.py
@@ -1,70 +1,93 @@
 #!/usr/bin/env python3
 
+# exit
 import sys
-
-# global variable
-danbooru_tags = list()
-yandere_tags = list()
+# isfile
+import os
+# config
+import json
+# subprocess
+import subprocess
 
 def main(argv):
     """
     main loop
     """
     # TODO(LuHa): print message about program execution
-    print('Execute wallpaper manager')
+    print('[WM] Execute wallpaper manager')
 
-    # input variable for user
-    user_input = None
+    # TODO(LuHa): restore tags
+    if os.path.isfile('tags'):
+        with open('tags', 'r') as fp_tags:
+            tags = json.load(fp_tags)
+    else:
+        tags = dict()
+        tags['danbooru'] = list()
+        tags['yandere'] = list()
 
     # TODO(LuHa): junction loop according users' input
-    while(True):
-        print_menu()
+    while True:
+        print('\n----+----+----+ Main menu ----+----+----+')
+        print('1. Edit danbooru search tags')
+        print(tags['danbooru'])
+        print('2. Edit yandere search tags')
+        print(tags['yandere'])
+        print('3. Start download wallpaper')
+        print('q. Terminate programm')
+        print('----+----+----+----+----+----+----+----+')
         user_input = input('User input: ')
-        if(user_input is None):
-            continue
         user_input = user_input.lower()
         # TODO(LuHa): jump to function
-        if(user_input == '1'):
-            edit_danbooru_tags()
-        elif(user_input == '2'):
-            edit_yandere_tags()
-        elif(user_input == '3'):
+        if user_input == '1':
+            edit_tags(tags, 'danbooru')
+        elif user_input == '2':
+            edit_tags(tags, 'yandere')
+        elif user_input == '3':
             start_download()
-        elif(user_input == 'q'):
+        elif user_input == 'q':
             break
 
     # TODO(LuHa): print message about program termination
-    print('Terminate wallpaper manager')
+    print('[WM] Terminate wallpaper manager')
     
 
-def print_menu():
-    print('----+----+----+ Main menu ----+----+----+')
-    print('1. Edit danbooru search tags')
-    print(danbooru_tags)
-    print('2. Edit yandere search tags')
-    print(yandere_tags)
-    print('3. Start download wallpaper')
-    print('q. Terminate programm')
-    print('----+----+----+----+----+----+----+----+')
-
-
-def edit_danbooru_tags():
+def edit_tags(tags, key):
     """
+    edit tag function
     """
-    print('danbooru')
-
-
-def edit_yandere_tags():
-    """
-    """
-    print('yandere')
+    # TODO(LuHa): junction loop
+    while(True):
+        print('\n----+----+ Edit tags ----+----+')
+        print('Current tag list')
+        print(tags[key])
+        print('a. Add tag')
+        print('d. Delete tag')
+        print('b. back')
+        print('----+----+----+----+')
+        user_input = input('User input: ')
+        user_input = user_input.lower()
+        # TODO(LuHa): processing user's input
+        if user_input == 'a':
+            user_input = input('Tag to add: ')
+            tags[key].append(user_input)
+        if user_input == 'd':
+            user_input = input('Tag to delete: ')
+            if user_input in tags:
+                tags[key].remove(user_input)
+        if user_input == 'b':
+            break
+    # TODO(LuHa): save tags
+    with open('tags', 'w') as fp_tags:
+        tags[key].sort()
+        json.dump(tags, fp_tags,
+                  indent = 4,
+                  sort_keys = True)
 
 
 def start_download():
     """
     """
-    print('download')
-
+    subprocess.run(['python3', 'image_downloader.py'])
 
 
 if __name__ == '__main__':
