5be4b41745de2872061add2e305aa877155d0a0e
munhyunsu <munhyunsu@gmail.com>
2017-12-26 08:52:16 +0900

    python3

--- main/option_editor.py
+++ main/option_editor.py
@@ -6,6 +6,9 @@ def main(argv):
     print('[Option] Excute option editor')
     print('[Option] Terminate option editor')
 
-# is it good, right?
+# maybe it is good thing, right?
 if __name__ == '__main__':
+    if sys.version_info.major != 3:
+        print('[OptionEditor] Need python3')
+        sys.exit()
     sys.exit(main(sys.argv))
