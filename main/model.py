fe6ae7f74f069b4e47626577e2f8625a9ac76a81
munhyunsu <munhyunsu@gmail.com>
2018-03-18 08:45:21 +0900

    mvc

---
+++ main/model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+def main(argv):
+    # TODO(LuHa): Create controller
+
+    # TODO(LuHa): Create Model
+
+    # TODO(LuHa): Create View
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
