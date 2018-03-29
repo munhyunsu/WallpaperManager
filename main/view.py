62932e5fba99a4b869a0032d8b0cfeabbad80164
munhyunsu <munhyunsu@gmail.com>
2018-03-29 13:08:55 +0900

    view

--- main/view.py
+++ main/view.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import sys
 
+class WallpaperController(object):
+    def __init__(self):
+        pass
+
+
 def main(argv):
+    # TODO(LuHa): Create controller
+
+    # TODO(LuHa): Create Model
+
+    # TODO(LuHa): Create View
     pass
 
 
