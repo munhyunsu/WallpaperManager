48965e6987ba6947493c81971348a07bd98e54c6
munhyunsu <munhyunsu@gmail.com>
2018-05-03 09:55:59 +0900

    move logging package

--- main/global_variable.py
+++ main/global_variable.py
@@ -8,3 +8,24 @@ IMAGESOURCES = ['danbooru',
 
 # timeout
 TIMEOUT = 30
+
+# TODO(LuHa): This variable is shared across program.
+options = get_database('options.secret', across = True)
+# TODO(LuHa): set logger level
+# CRITICAL 50
+# ERROR 40
+# WARNING 30
+# INFO 20
+# DEBUG 10
+# NOTSET 0
+logging.basicConfig(format = 
+        '%(asctime)s:%(created)s:%(levelno)s:%(message)s',
+                    level = logging.NOTSET)
+logger = logging.getLogger('WM')
+logger.propagate = False
+file_handler = logging.FileHandler('wm.log')
+file_handler.setLevel(logging.ERROR)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(options['log'])
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
